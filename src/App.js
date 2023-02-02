0ab15ececf7c02ad587b33def8e0caa466a8fddc
pdiaz13 <prisciladiazva@gmail.com>
2023-02-02 17:53:53 -0300

    feat: add routing and display of notes in the note list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import logoNotes from './img/logoNotes.png';
-import NoteList from './components/NoteList/NoteList.js';
+import NoteList from './components/NoteList/NoteList';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className='aplication-notes'>
-			<div className='notes-logo-container'>
-				<img src={logoNotes} className='notes-logo' alt='logo' />
+		<Router>
+			<div className='aplication-notes'>
+				<div className='notes-logo-container'>
+					<img src={logoNotes} className='notes-logo' alt='logo' />
+				</div>
+				<div className='note-list'>
+					<h1> my notes</h1>
+					<br></br>
+					<Routes>
+						<Route path='/' element={<NoteList />} />
+					</Routes>
+				</div>
 			</div>
-			<div className='note-list'>
-				<h1> my notes</h1>
-				<br></br>
-				<NoteList />
-			</div>
-		</div>
+		</Router>
 	);
 }
 
